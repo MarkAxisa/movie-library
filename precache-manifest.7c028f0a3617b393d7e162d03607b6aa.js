db0cd3584d483f4b4a47cea308b70c8b447876b1
Mark Axisa <axisamark@hotmail.com>
2020-08-17 03:35:04 +0200

    Updates

---
+++ precache-manifest.7c028f0a3617b393d7e162d03607b6aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2a46a8e51961f351c267ed7ace9307",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "a9c80add80dd53270fb6",
+    "url": "/movie-library/static/css/main.fb05ca73.chunk.css"
+  },
+  {
+    "revision": "71b4066c28d1221a40a2",
+    "url": "/movie-library/static/js/2.368c714f.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/movie-library/static/js/2.368c714f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c80add80dd53270fb6",
+    "url": "/movie-library/static/js/main.ee349bd5.chunk.js"
+  },
+  {
+    "revision": "013c70500261c060a513",
+    "url": "/movie-library/static/js/runtime-main.cba2209a.js"
+  },
+  {
+    "revision": "84dcb9516229d4ccf755fe32b36eaf27",
+    "url": "/movie-library/static/media/drawerPlaceholder.84dcb951.jpg"
+  }
+]);
